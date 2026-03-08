e64cdb4b3bc0139f0a0a27bb62be4f4c19e30451
Anatoly Porshnyov <povly19995@gmail.com>
2026-03-08 21:32:55 +0300

    feat: add layouts:block-added event for dynamic content handling
    Dispatch custom event after block is added to DOM, allowing other
    packages (like moonshine-editorjs) to initialize their components.

--- public/js/layouts.js
+++ public/js/layouts.js
@@ -5,10 +5,10 @@ document.addEventListener('alpine:init', () => {
         root: null,
         blocksContainer: null,
         init() {
-            this.root = this.$root
-            this.blocksContainer = this.root.querySelector('._layouts-blocks')
-            this._reindex()
-            const t = this
+            this.root = this.$root;
+            this.blocksContainer = this.root.querySelector('._layouts-blocks');
+            this._reindex();
+            const t = this;
 
             MoonShine.iterable.sortable(
                 this.blocksContainer,
@@ -16,53 +16,63 @@ document.addEventListener('alpine:init', () => {
                 'layouts',
                 null,
                 {
-                    handle: '.handle'
+                    handle: '.handle',
                 },
-                function(evt) {
-                    t._reindex()
-                }
-            )
+                function (evt) {
+                    t._reindex();
+                },
+            );
         },
         add(name) {
-            const t = this
+            const t = this;
 
-            let layoutsCount = {}
-            const layouts = document.querySelectorAll('._layout-value')
-            layouts.forEach(function(l) {
-                layoutsCount[l.value] = layoutsCount[l.value] ? layoutsCount[l.value]+1 : 1
-            })
+            let layoutsCount = {};
+            const layouts = document.querySelectorAll('._layout-value');
+            layouts.forEach(function (l) {
+                layoutsCount[l.value] = layoutsCount[l.value] ? layoutsCount[l.value] + 1 : 1;
+            });
 
+            MoonShine.request(
+                t,
+                t.url,
+                'post',
+                {
+                    field: t.column,
+                    name: name,
+                    counts: layoutsCount,
+                },
+                {},
+                {
+                    afterResponse: function (data) {
+                        const tempContainer = document.createElement('div');
+                        tempContainer.innerHTML = data.html ?? data.htmlData[0].html ?? '';
 
-            MoonShine.request(t, t.url, 'post', {
-                field: t.column,
-                name: name,
-                counts: layoutsCount
-            }, {}, {
-                afterResponse: function(data) {
-                    const tempContainer = document.createElement('div');
-                    tempContainer.innerHTML = data.html ?? data.htmlData[0].html ?? '';
+                        while (tempContainer.firstChild) {
+                            t.blocksContainer.appendChild(tempContainer.firstChild);
+                        }
 
-                    while (tempContainer.firstChild) {
-                        t.blocksContainer.appendChild(tempContainer.firstChild);
-                    }
+                        t._reindex();
 
-                    t._reindex()
-                }
-            })
+                        // Событие для отслеживания добавления нового блока
+                        document.dispatchEvent(
+                            new CustomEvent('layouts:block-added', {
+                                detail: { name: name, column: t.column },
+                            }),
+                        );
+                    },
+                },
+            );
         },
         remove() {
-            this.$el.closest('._layouts-block').remove()
-            this._reindex()
+            this.$el.closest('._layouts-block').remove();
+            this._reindex();
         },
         _reindex() {
-            const t = this
+            const t = this;
 
-            this.$nextTick(function() {
-                MoonShine.iterable.reindex(
-                    t.blocksContainer,
-                    '._layouts-block'
-                )
-            })
-        }
-    }))
-})
+            this.$nextTick(function () {
+                MoonShine.iterable.reindex(t.blocksContainer, '._layouts-block');
+            });
+        },
+    }));
+});
